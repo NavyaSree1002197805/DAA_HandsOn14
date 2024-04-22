2efb85ebeab716d07bd11b779e0ccde960456d28
NavyaSree1002197805 <158221388+NavyaSree1002197805@users.noreply.github.com>
2024-04-21 20:24:58 -0500

    Create KruskalAlgo.py

---
+++ KruskalAlgo.py
@@ -0,0 +1,88 @@
+from collections import defaultdict
+
+class KruskalGraph:
+    def __init__(self, num_vertices):
+        self.num_vertices = num_vertices
+        self.graph = []
+
+    def add_edge(self, u, v, weight):
+        self.graph.append([u, v, weight])
+
+    def find_parent(self, parent, i):
+        if parent[i] == i:
+            return i
+        return self.find_parent(parent, parent[i])
+
+    def union(self, parent, rank, x, y):
+        x_root = self.find_parent(parent, x)
+        y_root = self.find_parent(parent, y)
+
+        if rank[x_root] < rank[y_root]:
+            parent[x_root] = y_root
+        elif rank[x_root] > rank[y_root]:
+            parent[y_root] = x_root
+        else:
+            parent[y_root] = x_root
+            rank[x_root] += 1
+
+    def kruskal_mst(self):
+        result = []
+        i = 0
+        e = 0
+        self.graph = sorted(self.graph, key=lambda item: item[2])
+        parent = []
+        rank = []
+
+        for node in range(self.num_vertices):
+            parent.append(node)
+            rank.append(0)
+
+        while e < self.num_vertices - 1:
+            u, v, w = self.graph[i]
+            i += 1
+            x = self.find_parent(parent, u)
+            y = self.find_parent(parent, v)
+
+            if x != y:
+                e += 1
+                result.append([u, v, w])
+                self.union(parent, rank, x, y)
+
+        return result
+
+# Test the algorithm
+vertex_labels = {
+    0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e',
+    5: 'f', 6: 'g', 7: 'h', 8: 'i'
+}
+
+g = KruskalGraph(9)
+g.add_edge(0, 1, 4)  # a-b
+g.add_edge(0, 7, 8)  # a-h
+g.add_edge(1, 7, 11)  # b-h
+g.add_edge(1, 2, 8)  # b-c
+g.add_edge(7, 8, 7)  # h-i
+g.add_edge(6, 7, 1)  # g-h
+g.add_edge(6, 8, 6)  # g-i
+g.add_edge(2, 8, 2)  # c-i
+g.add_edge(2, 5, 4)  # c-f
+g.add_edge(2, 3, 7)  # c-d
+g.add_edge(5, 3, 14)  # f-d
+g.add_edge(5, 4, 10)  # f-e
+g.add_edge(3, 4, 9)  # d-e
+
+mst = g.kruskal_mst()
+
+print("Minimum Spanning Tree:")
+for u, v, w in mst:
+    print(f"{vertex_labels[u]} - {vertex_labels[v]} = {w}")
+
+'''Output:
+g - h = 1
+c - i = 2
+a - b = 4
+c - f = 4
+g - i = 6
+c - d = 7
+a - h = 8
+d - e = 9 '''
