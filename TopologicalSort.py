756b072b96abac87e5ed5f4c20556949764d79e1
NavyaSree1002197805 <158221388+NavyaSree1002197805@users.noreply.github.com>
2024-04-21 19:22:37 -0500

    Create TopologicalSort.py

---
+++ TopologicalSort.py
@@ -0,0 +1,50 @@
+from collections import defaultdict
+
+class TopologicalGraph:
+    def __init__(self):
+        self.graph = defaultdict(list)
+
+    def add_edge(self, u, v):
+        self.graph[u].append(v)
+
+    def topological_sort(self):
+        vertices = ['shirt', 'tie', 'socks', 'pants', 'undershorts', 'belt', 'jacket', 'shoes']
+        in_degree = {u: 0 for u in vertices}
+        for u in self.graph:
+            for v in self.graph[u]:
+                in_degree[v] += 1
+
+        queue = [u for u in vertices if in_degree[u] == 0]
+        topological_order = []
+
+        while queue:
+            u = queue.pop(0)
+            topological_order.append(u)
+            for v in self.graph[u]:
+                in_degree[v] -= 1
+                if in_degree[v] == 0:
+                    queue.append(v)
+
+        if any(in_degree.values()):
+            raise ValueError("Graph contains a cycle")
+
+        return topological_order
+
+# Testing the algorithm with the book example
+g = TopologicalGraph()
+g.add_edge('undershorts', 'socks')
+g.add_edge('undershorts', 'pants')
+g.add_edge('pants', 'belt')
+g.add_edge('socks', 'belt')
+g.add_edge('shirt', 'tie')
+g.add_edge('tie', 'undershorts')
+g.add_edge('belt', 'jacket')
+g.add_edge('jacket', 'shoes')
+
+print("Topological Ordering:")
+print(g.topological_sort())
+
+'''Output:
+Topological Ordering:
+['shirt', 'tie', 'undershorts', 'socks', 'pants', 'belt', 'jacket', 'shoes']
+'''
